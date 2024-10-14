7d4299d2e71ac64385ae05f768bc46325d49a645
AnnaKharatova <black-isida@yandex.ru>
2024-10-15 00:35:25 +0300

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App() {
     <div className='w-11/12 mx-auto border-2 border-solid border-white rounded-2xl mt-20 p-3  sm:w-2/3 mb-8' >
       <Header />
       <Routes>
-        <Route path="/" Component={SearchPage} />
-        <Route path="/person/:id" Component={PersonPage} />
+        <Route path="rick-and-morty-universe/" Component={SearchPage} />
+        <Route path="rick-and-morty-universe/person/:id" Component={PersonPage} />
       </Routes>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/AnnaKharatova/rick-and-morty-universe.git'
+  base: '/AnnaKharatova/rick-and-morty-universe.git',
+  build: {
+    outDir: 'build', // Изменяем outDir на 'build'
+   }
 })
+
